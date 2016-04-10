a84150655b460499bfd0f7e8404dfc8ec339bb5b
Sandro Micalizzi <contact@sandromicalizzi.com>
2016-04-10 13:29:17 +0200

    super commit de la mort qui tue

---
+++ js/homepage.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('#question1').hide();
+  $('#question3').hide();
+  $('#question4').hide();
+
+  $('.avatar').click(function(){
+    $('#login').fadeOut('fast');
+    $('#head').slideUp('slow');
+    $('#head').removeClass('load');
+    $('#head').addClass('header');
+    $('#head').slideDown('fast');
+    $('#homepage').fadeOut('fast');
+    $('#question1').show();
+  });
+
+  $('#q1').click(function(){
+    $('#question1').fadeOut('fast');
+    $('#question3').show().fadeIn('fast');
+  });
+
+  $('#q3').click(function(){
+    $('#question3').fadeOut('fast');
+    $('#question4').show().fadeIn('fast');
+  });
+});
